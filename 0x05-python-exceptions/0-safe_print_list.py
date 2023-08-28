2804de51686c3a2aa94b414f43849132c46000b6
DexyMaroni <divineinyang7@gmail.com>
2023-08-28 12:21:27 -0700

    hmm

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[c], end="")
             a += 1
         except IndexError:
-             continue
+            break
     print()
-    return n
+    return a
