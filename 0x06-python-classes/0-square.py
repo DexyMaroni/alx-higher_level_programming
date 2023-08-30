a1a77f71ed4533652199295e853d7fc4091c74f3
DexyMaroni <divineinyang7@gmail.com>
2023-08-30 14:28:43 -0700

    Square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''A Class that describes a square'''
+"""A Class that describes a square"""
 
 class Square:
-    ''' Represents a square'''
+    """Represents a square"""
     pass
 
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A class that Describes a square"""
+class Square:
+    def __init__(self, size):
+        """Initializes a square
+
+        Args:
+            size (int) The Size of the new square.
+
+        """
+
+    self.__size = size
+
